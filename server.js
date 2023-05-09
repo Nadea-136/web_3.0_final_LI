668710e0bcb65b91d7b63fdafeca19903e8fe391
”Your <grubneac.nadea@gmai.com>
2023-05-09 15:54:33 +0300

    k

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+
+const server = http.createServer((req, res) => {
+    switch(req.url) {
+        case '/': return res.end(indexHtmlFile);
+        case '/script.js': return res.end(scriptFile);
+        case '/style.css': return res.end(styleFile);
+    }
+    return res.end('Error 404');
+});
+
+
+server.listen(5000);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected. id - ' + socket.id);
+
+  socket.on('new_message', (message) => {
+    console.log(message);
+      io.emit('message', message);
+  });
+});
